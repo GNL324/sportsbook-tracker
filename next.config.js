73f88347d5685400e701b60b8ad693d06718a8d1
Noel <lng.ver02@gmail.com>
2026-04-06 16:59:47 -0400

    Configure for GitHub Pages static deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/sportsbook-tracker',
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
